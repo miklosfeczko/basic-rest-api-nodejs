466771a8b1c17eba49a8867064b4ee3dcad17b25
Miklos Feczko <miklosfeczko01@gmail.com>
2020-01-02 18:25:40 +0100

    Created basic handles.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routes = require('./routes/api')
+
+// set up express app
+const app = express()
+
+// initialize routes
+app.use('/api', routes)
+
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+  console.log('now listening for requests')
+})
+
