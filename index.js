5a97e2bff23d5ec93cf3cce9018f1a5c0691adb8
Miklos Feczko <miklosfeczko01@gmail.com>
2020-01-03 22:01:59 +0100

    Added body-parser middleware and extended post request.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const routes = require('./routes/api')
+const bodyParser = require('body-parser')
 
 // set up express app
 const app = express()
+
+// middleware
+// its going to parse the json data for us. Attach it to the request object so by the time
+// it reaches our route handler it will have access to it
+app.use(bodyParser.json())
 
 // initialize routes
 app.use('/api', routes)
